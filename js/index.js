df368179081c9936aa2abf503a7767e6bcc8d2fd
Duy-Khang0607 <nguyenhoangduykhang0607@gmail.com>
2022-11-16 23:46:11 +0700

    23

--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@ function dataFirebase(Rain, servo, lamp, motor, soil, temp) {
 
 firebase
   .database()
-  .ref()
+  .ref("Garden")
   .on("value", (snap) => {
     var rain = snap.val().Rain;
     var servo = snap.val().servo;
@@ -56,13 +56,13 @@ firebase
 var soil = document.querySelector("#valueSoil");
 var temp = document.querySelector("#valueTemperature");
 var database = firebase.database();
-database.ref().on("value", (snap) => {
+database.ref("Garden").on("value", (snap) => {
   valueSoil = snap.val().soil;
   soil.innerHTML = snap.val().soil;
   valueTemp = snap.val().temp;
   temp.innerHTML = snap.val().temp;
-  firebase.database().ref("Garden").set(valueSoil);
-  firebase.database().ref("Garden").set(valueTemp);
+  firebase.database().ref().set(valueSoil);
+  firebase.database().ref().set(valueTemp);
   console.log(soil, "soil");
   console.log(temp, "temp");
 });
